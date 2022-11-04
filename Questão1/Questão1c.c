6257d9c263aa58b53982e1f27aad5dc6a75bcc6b
Geovana Beatriz <107216962+Calopsitaaaa@users.noreply.github.com>
2022-11-03 21:30:17 -0300

    Questão1

---
+++ Questão1/Questão1c.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+#include <stdlib.h>
+int main(){
+    int numA, numB;
+    printf("coloque o primeiro numero");
+    scanf("%d", &numA);
+    printf("coloque o segundo numero");
+    scanf("%d", &numB);
+    if(numA>numB){
+        printf("%d", numA);
+    else}
+    (numB>numA)
+        printf("%d", numB);
+    }
+    system("PAUSE");
+}
