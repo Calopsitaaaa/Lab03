3f71fb7211a626b034e48010339282f27836021f
Geovana Beatriz <107216962+Calopsitaaaa@users.noreply.github.com>
2022-11-03 21:32:18 -0300

    Questão2

---
+++ Questão2/Questão2c.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <math.h>
+
+int main()
+{
+float num, ra;
+printf("digite um número: ");
+scanf("%f", &num);
+if(num >=0)
+{
+raiz = sqrt(num);
+printf("raiz: %f", rai;
+}
+else
+{
+printf("numero inválido");
+}
+return 0};
